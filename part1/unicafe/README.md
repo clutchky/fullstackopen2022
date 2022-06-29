80c04cb7b28473002f92a2ef64801496228d6be6
Jam Amoroso <clutchky@gmail.com>
2022-06-29 22:45:22 +0800

    added unicafe exercise

---
+++ part1/unicafe/README.md
@@ -0,0 +1,3 @@
+# Unicafe Exercise
+
+Part 1 Exercises (1.6-1.11) of Full Stack Open 2022
---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,90 @@
+import { useState } from "react";
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const StatisticLine = ({text, value}) => {
+
+  if (text === 'positive') {
+    return (
+      <tr>
+        <td>{text}</td>
+        <td>{value} %</td>
+      </tr>
+    )
+  }
+
+  return (
+        <tr>
+          <td>{text}</td>
+          <td>{value}</td>
+        </tr>
+  )
+}
+
+const Statistics = (props) => {
+  
+  const {good, neutral, bad} = props;
+
+  const total = (good + bad + neutral);
+  const average = ((good - bad) / total).toFixed(1);
+  const positive = ((good / total) * 100).toFixed(1);
+
+  if (total > 0){
+    return (
+      <div>
+        <h1>statistics</h1>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={total} /> 
+            <StatisticLine text="average" value={average} /> 
+            <StatisticLine text="positive" value={positive} />
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+
+  return (
+  <div>
+    <p>No feedback given</p>
+  </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+    setGood(good + 1);
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1);
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGood} text='good'/>
+      <Button handleClick={handleNeutral} text='neutral'/>
+      <Button handleClick={handleBad} text='bad'/>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App;
