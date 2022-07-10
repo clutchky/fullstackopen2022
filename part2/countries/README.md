1937a0ef5af4b07cc86b85f8b16310730bf9eb51
Jam Amoroso <clutchky@gmail.com>
2022-07-11 01:41:08 +0800

    added countries exercise

---
+++ part2/countries/README.md
@@ -0,0 +1,3 @@
+# Countries Exercise
+
+Part 2 Exercises (2.12 - 2.14) of Full Stack Open 2022
---
+++ part2/countries/src/App.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from 'react';
+
+import axios from 'axios';
+
+const App = () => {
+
+  const [countries, setCountries] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
+  useEffect(() => {
+    console.log('getting data');
+
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        console.log('data found');
+        console.log(response.data);
+
+      setCountries(response.data);
+      })
+  }, []);
+
+  const searchedCountry = countries.filter(country => (
+    country.name.common.toLowerCase().includes(searchValue.toLowerCase())
+  ))
+
+  const displayOne = searchValue !== '' && searchedCountry.length === 1
+  const exceedLimit = searchedCountry.length > 10 && searchValue !== ''
+  const withinLimit = searchedCountry.length < 10
+
+  const handleInputChange = (event) => {
+    setSearchValue(event.target.value);
+  }
+
+  return (
+    <div>
+      <div>
+        find countries <input value={searchValue} onChange={handleInputChange}/>
+      </div>
+      <div>
+        {displayOne
+        ? searchedCountry.map(country => ( // display one country
+          <div key={country.name.common}>
+            <h1>{country.name.common}</h1>
+            <p>Capital: {country.capital}</p>
+            <p>Area: {country.area}</p>
+            <h3>languages</h3>
+            <ul>
+            {Object.values(country.languages).map((lang, i) => <li key={i}>{lang}</li>)}
+            </ul>
+            <img src={country.flags.svg} alt={country.name.common + '\'s flag'} width="150"/>
+          </div>)
+        )
+        : exceedLimit
+        ? 'Too many matches, specify another filter'
+        : withinLimit
+        ? searchedCountry.map((country) => // default country list
+        <p key={country.name.common}>{country.name.common}</p>
+        )
+        : '' // default state
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App;
