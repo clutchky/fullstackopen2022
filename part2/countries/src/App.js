0eca86729d620a796642812dd67179e7ae99fb01
Jam Amoroso <clutchky@gmail.com>
2022-07-14 10:05:23 +0800

    added solution to 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -9,9 +9,17 @@ const App = () => {
 
   const [countries, setCountries] = useState([]);
   const [searchValue, setSearchValue] = useState('');
+  const [weatherData, setWeatherData] = useState([]);
+  const [capital, setCapital] = useState('');
 
+  // filter country
+  const searchedCountry = countries.filter((country) => (
+    country.name.common.toLowerCase().includes(searchValue.toLowerCase())
+  ))
+
+  // get country data
   useEffect(() => {
-    console.log('getting data');
+    console.log('getting countries data');
 
     axios
       .get('https://restcountries.com/v3.1/all')
@@ -22,19 +30,55 @@ const App = () => {
       })
   }, []);
 
-  const searchedCountry = countries.filter(country => (
-    country.name.common.toLowerCase().includes(searchValue.toLowerCase())
-  ))
+  // get weather data
+  useEffect(() => {
+
+      const api_key = process.env.REACT_APP_API_KEY;
+
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api_key}&units=metric`
+
+      if (capital === '') {
+        url = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${api_key}&units=metric`
+      
+        axios
+        .get(url)
+        .then(response => {
+          console.log('weather data found');
+
+          setWeatherData(response.data);
+        })
+      } else {
+      axios
+        .get(url)
+        .then(response => {
+          console.log('weather data found');
+
+          setWeatherData(response.data);
+        })
+      }
+    
+  }, [capital])
+
 
   const displayOne = searchedCountry.length === 1
-  const exceedLimit = searchedCountry.length > 10 
+  const exceedLimit = searchedCountry.length > 10
 
-  const handleInputChange = (event) => {
+
+  const handleInputChange = (event) => {     
     setSearchValue(event.target.value);
+
+    if (displayOne) {
+      setCapital(searchedCountry[0].capital);
+    }
+
   }
 
   const handleClick = (name) => {
     setSearchValue(name)
+
+    if (displayOne) {
+      setCapital(searchedCountry[0].capital);
+    }
   }
 
   return (
@@ -44,7 +88,7 @@ const App = () => {
       </div>
       <div>
         {displayOne
-        ? <Country countries={searchedCountry} />
+        ? <Country countries={searchedCountry} weatherData={weatherData}/>
         : exceedLimit
         ? 'Too many matches, specify another filter'
         : <CountryList countries={searchedCountry} showDetails={handleClick}/>
--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,18 +1,22 @@
 
-const Country = ({countries}) => {
+const Country = ({countries, weatherData}) => {
 
     return (
         <>
           {countries.map(country => ( // display one country
             <div key={country.name.common}>
               <h1>{country.name.common}</h1>
-              <p>Capital: {country.capital}</p>
+              <p>Capital: {country.capital[0]}</p>
               <p>Area: {country.area}</p>
               <h3>languages</h3>
               <ul>
               {Object.values(country.languages).map((lang, i) => <li key={i}>{lang}</li>)}
               </ul>
               <img src={country.flags.svg} alt={country.name.common + '\'s flag'} width="150"/>
+              <h3>Weather in {country.capital}</h3>
+              <p>temperature is {weatherData.main.temp} Celsius</p>
+              <img src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt={weatherData.weather.icon}/>
+              <p>wind {weatherData.wind.speed} m/s</p>
             </div>)
             )
           }
