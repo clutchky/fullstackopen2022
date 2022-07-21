3d70833a7bb9b431c3791eeb0f239c21e3107fa7
Jam Amoroso <clutchky@gmail.com>
2022-07-21 08:00:51 +0800

    added solution to 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -28,7 +28,15 @@ let persons = [
 
 app.use(express.json())
 
-app.use(morgan('tiny'))
+// create body token
+morgan.token('body', (req, res) => {
+    return JSON.stringify(req.body);   
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
+// Routes
 
 // Phonebook root
 app.get('/', (request, response) => {
