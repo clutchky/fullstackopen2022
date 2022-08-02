3c1d2fdff59afd28256bc642d87d6737cf0bbd36
Jam Amoroso <clutchky@gmail.com>
2022-08-03 02:27:36 +0800

    added solution to 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -4,6 +4,27 @@ const dummy = (blogs) => {
   }
 };
 
+const totalLikes = (blogs) => {
+
+  if (blogs.length === 0) {
+    return 0;
+  }
+
+  // if list has only one blog, total likes = blog.likes
+  if (blogs.length === 1) {
+    return blogs[0].likes;
+  }
+
+  // if many blogs, total all likes
+  const reducer = (sum, blogs) => {
+    return sum + blogs.likes;
+  }
+
+  return blogs.reduce(reducer, 0);
+  
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
