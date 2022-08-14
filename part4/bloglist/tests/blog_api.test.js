041bf85d42eb96b2b892a4200cfd7be7b1fb726a
Jam Amoroso <clutchky@gmail.com>
2022-08-15 04:43:33 +0800

    added solution to 4.12

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -94,6 +94,21 @@ test('likes property defaults to zero if not defined', async () => {
   expect(likesProperty).toBe(0);
 });
 
+test('missing title and url is not added', async () => {
+  const newBlog = {
+    author: 'Anonymous'
+  };
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400);
+
+  const response = await api.get('/api/blogs');
+
+  expect(response.body).toHaveLength(initialBlogs.length);
+});
+
 afterAll(() => {
   mongoose.connection.close();
 })
