9967a810bfabcf10b5a3af873389213f4acae21e
Jam Amoroso <clutchky@gmail.com>
2022-08-21 05:25:45 +0800

    added solution to 4.15

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,9 +1,11 @@
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const helper = require('../tests/test_helper');
 const app = require('../app');
 const api = supertest(app);
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 beforeEach( async () => {
   await Blog.deleteMany({});
