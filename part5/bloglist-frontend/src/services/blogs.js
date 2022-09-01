42c6c38cd757784509e7132853510e78b1ad1ba4
Jam Amoroso <clutchky@gmail.com>
2022-09-01 23:04:21 +0800

    added solution to 5.1

---
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+const baseUrl = '/api/blogs'
+
+// let token = null;
+
+// const setToken = newToken => {
+//   token = `bearer ${newToken}`;
+// }
+
+const getAll = async () => {
+  const response = await axios.get(baseUrl);
+
+  return response.data
+}
+
+const blogService = { getAll }
+export default blogService
