1cb731fd8bf408eabe6e8ac79148b73428f401db
Jam Amoroso <clutchky@gmail.com>
2022-12-02 12:16:40 +0800

    added solution to 8.4

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -111,7 +111,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -120,7 +120,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if(!args.author) {
+        return books
+      }
+      const book = books.filter(b => b.author === args.author)
+      return book
+    },
     allAuthors: () => authors
   },
   Author: {
