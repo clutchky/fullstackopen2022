3338b535d446656471aa5b02e0a4d9b8813bac34
Jam Amoroso <clutchky@gmail.com>
2023-01-12 06:41:11 +0800

    added solution to 8.17

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -27,7 +27,7 @@ const Books = (props) => {
           {books.data.allBooks.map((b) => (
             <tr key={b.title}>
               <td>{b.title}</td>
-              <td>{b.author}</td>
+              <td>{b.author ? b.author.name : ""}</td>
               <td>{b.published}</td>
             </tr>
           ))}
